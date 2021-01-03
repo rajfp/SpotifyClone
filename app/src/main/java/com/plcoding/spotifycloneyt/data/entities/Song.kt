2a533f1d5b6870f1ac6b3a89c70b013788e62f7d
Rajat Chauhan <rajat1.chauhan@airtelbank.com>
2021-01-03 13:02:25 +0530

    added firebase connection to app

---
+++ app/src/main/java/com/plcoding/spotifycloneyt/data/entities/Song.kt
@@ -0,0 +1,9 @@
+package com.plcoding.spotifycloneyt.data.entities
+
+class Song(
+    val mediaId: String = "",
+    val title: String = "",
+    val subtitle: String = "",
+    val songUrl: String = "",
+    val imageUrl: String = ""
+)
